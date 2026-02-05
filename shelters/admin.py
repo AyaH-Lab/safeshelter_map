7e9ac40d63a77690bfa9672d0e04ebc015872d02
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-02-05 14:21:49 +0900

    Placeモデルを管理画面に登録するPlaceAdminを追加

--- shelters/admin.py
+++ shelters/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+# models.pyからPlaceモデルをインポートする
+from .models import Place
+
+# PlaceモデルをDjango管理画面に登録するデコレータ
+@admin.register(Place)
+
+class PlaceAdmin(admin.ModelAdmin):
+    """Placeモデルをカスタマイズするためのクラス"""
+
+    # 管理画面の一覧ページに表示するフィールドを指定する
+    list_display = ("category", "name", "address", "source")
+
+    # 管理画面の検索ボックスで検索対象にするフィールドを指定する
+    search_fields = ("name", "address")
+
+    # 右側に表示されるフィルター(絞り込み)項目を指定
+    list_filter = ("category", "source")
