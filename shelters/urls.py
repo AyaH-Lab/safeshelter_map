8b3c67ce1c72eacd16be7093f58486a6493f3aaf
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-02-09 13:15:57 +0900

    shelters/urls.py作成

---
+++ shelters/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+# URLの名前空間を設定。テンプレートで'shelters:  'みたいに使えるようになる
+app_name = "shelters"
+
+# URLパターンをまとめたリスト(Djangoがここを読み取る)
+urlpatterns = [
+    # /places/にアクセスしたらviews.place_listを実行
+    # name="place_list"はテンプレートやrevers()で使う名前
+    path("places/", views.place_list, name="place_list"),
+
+    # /places/にアクセスしたらviews.place_detailを実行
+    # <int:pk>はURLの数字部分をpkとしてレビューに渡す
+    path("places/<int:pk>", views.place_detail, name="place_detail"),
+]
