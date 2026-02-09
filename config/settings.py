9481266eed16c38922166b1394019df06bde50f6
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-02-09 13:54:01 +0900

    templates検索パス追加

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"], # templates検索パス
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
